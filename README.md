89fa29fd5016ccab9a6c5d033bde622220f646bd
Usitha Indeewara <96857837+usithadev@users.noreply.github.com>
2023-06-16 15:07:41 +0000

    Pending changes exported from your codespace

---
+++ README.md
@@ -0,0 +1,8 @@
+# WhatsApp flooding user bot
+
+Note: For educational purposes only  
+
+This is a code for a whatsapp userbot that send flood messages. After running `npm start` you can scan the qr code on the terminal by going to Whatsapp-> Linked devices.
+After that you may find a text `bot is ready` in the terminal. (You can test it by sending `!test` in any chat on your whatsapp). Now you can go to any chat on your whatsapp and send flood messages by the command `!flood [number of messages] [text]`. (eg: `!flood 30 Hello!`).  
+
+PLEASE DON'T DO THIS TO ANYONE! FLOOD MESSAGES ARE ANNOYING! USE THIS AT YOUR OWN RISK! IF YOU DO THIS, THERE IS A HIGHER CHANCE OF YOUR WHATSAPP ACCOUNT GETTING BANNED! USE THIS CODE ONLY FOR EDUCATIONAL PURPOSES!!!
---
+++ index.ts
@@ -0,0 +1,31 @@
+// Copyright (c) 2023 Usitha Indeewara.
+
+import { Client } from "whatsapp-web.js";
+import code from "qrcode-terminal"
+const client = new Client({});
+
+client.on("qr", (cod) => {
+    code.generate(cod, {small: true});
+});
+
+client.on("ready", () => console.log("The bot is ready!"));
+
+function log(txt:string) {
+    console.log(new Date() + " : " + txt);
+}
+
+client.on("message_create", (msg) => {
+    if(msg.body.includes("!test")) {
+        msg.reply("Working!");
+    } else if(msg.body.match(/\!flood (\d+) (.+)/i)) {
+        let reg = msg.body.match(/\!flood (\d+) (.+)/i) || [];
+        const it:number = Number(reg[1]);
+        const txt:string = reg[2];
+
+        for(let x = 0; x <= it; x++) {
+            msg.reply(txt);
+        }
+    }
+})
+
+client.initialize();
